cc195ebd6e4a055da5110ea70862236a5f1db968
Piotr Kozka <piotoor@gmail.com>
2021-07-13 00:26:46 +0200

    Added test_2x2 to BinarizedImageToPCodesTests

--- printer_tests.py
+++ printer_tests.py
@@ -75,3 +75,287 @@ class BinarizedImageToPCodesTests(unittest.TestCase):
         for images, expected in zip(binarized_testcases, expected_p_codes):
             p_codes = printer.binarized_image_to_p_codes(images, x_res, y_res)
             self.assertEqual(p_codes, expected)
+
+    def test_2x2(self):
+        x_res = 2
+        y_res = 2
+
+        binarized_testcases = [
+            [False, False,
+             False, False],
+
+            [False, False,
+             False, True],
+
+            [False, False,
+             True, False],
+
+            [False, False,
+             True, True],
+            # --------------------------------
+            [False, True,
+             False, False],
+
+            [False, True,
+             False, True],
+
+            [False, True,
+             True, False],
+
+            [False, True,
+             True, True],
+            # --------------------------------
+            [True, False,
+             False, False],
+
+            [True, False,
+             False, True],
+
+            [True, False,
+             True, False],
+
+            [True, False,
+             True, True],
+            # --------------------------------
+            [True, True,
+             False, False],
+
+            [True, True,
+             False, True],
+
+            [True, True,
+             True, False],
+
+            [True, True,
+             True, True]
+        ]
+
+        expected_p_codes = [
+            [
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+            ],
+
+            [
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 0],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+            ],
+
+            [
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 0],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+            ],
+
+            [
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+            ],
+            # ------------------------------------------------------------
+            [
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 0],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+            ],
+
+            [
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 0],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 0],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+            ],
+
+            [
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 0],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 0],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+            ],
+
+            [
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 0],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+            ],
+            # ------------------------------------------------------------
+            [
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 0],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+            ],
+
+            [
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 0],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 0],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+            ],
+
+            [
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 0],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 0],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+            ],
+
+            [
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 0],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+            ],
+            # ------------------------------------------------------------
+            [
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+            ],
+
+            [
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 0],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+            ],
+
+            [
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 0],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+            ],
+
+            [
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+
+                [printer.Command.PEN_DOWN, 0],
+                [printer.Command.PEN_RIGHT, 1],
+                [printer.Command.PEN_UP, 0],
+                [printer.Command.PEN_LEFT, x_res - 1],
+                [printer.Command.SCROLL, 1],
+            ]
+        ]
+
+        for images, expected in zip(binarized_testcases, expected_p_codes):
+            p_codes = printer.binarized_image_to_p_codes(images, x_res, y_res)
+            self.assertEqual(p_codes, expected)

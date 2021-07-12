0d6524c33466c750f2e0a6c482bfbef2ec1600c4
Piotr Kozka <piotoor@gmail.com>
2021-07-12 23:31:25 +0200

    Minor code formatting improvements

--- printer_tests.py
+++ printer_tests.py
@@ -31,11 +31,12 @@ class BinarizedImageToPCodesTests(unittest.TestCase):
         x_res = 2
         y_res = 1
 
-        binarized_testcases = [[True, True],
-                               [True, False],
-                               [False, True],
-                               [False, False]
-                               ]
+        binarized_testcases = [
+            [True, True],
+            [True, False],
+            [False, True],
+            [False, False]
+        ]
 
         expected_p_codes = [
             [
@@ -74,18 +75,3 @@ class BinarizedImageToPCodesTests(unittest.TestCase):
         for images, expected in zip(binarized_testcases, expected_p_codes):
             p_codes = printer.binarized_image_to_p_codes(images, x_res, y_res)
             self.assertEqual(p_codes, expected)
-
-    # def test_black_square(self):
-    #     binarized = []
-    #     x_res = 5
-    #     y_res = 5
-    #     p_codes = printer.binarized_image_to_p_codes(binarized, x_res, y_res)
-    #     self.expected_p_codes = [
-    #
-    #     ]
-    #     self.assertEqual(p_codes, self.expected_p_codes)
-
-    # test_white_square
-    # test_alternating_white_and_black_rows
-    # test_alternating_white_and_black_columns
-    # test_chessboard

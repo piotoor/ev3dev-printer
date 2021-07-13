b82033fe7ea1ca57ddf3e52cdb138fe3a6c583ee
Piotr Kozka <piotoor@gmail.com>
2021-07-14 00:57:28 +0200

    Added test_10x10_alternating_cols to BinarizedImageToPCodesTests

--- utilities_tests.py
+++ utilities_tests.py
@@ -381,3 +381,42 @@ class BinarizedImageToPCodesTests(unittest.TestCase):
 
         p_codes = utilities.binarized_image_to_p_codes(binarized, x_res, y_res)
         self.assertEqual(p_codes, expected_p_codes)
+
+    def test_10x10_alternating_cols(self):
+        x_res = 10
+        y_res = 10
+
+        binarized = [
+            x % 2 == 0 for x in range(x_res * y_res)  # works only for even x_res
+        ]
+
+        expected_p_codes = [
+                [utilities.Command.PEN_DOWN, 0],
+                [utilities.Command.PEN_RIGHT, 0],
+                [utilities.Command.PEN_UP, 0],
+                [utilities.Command.PEN_RIGHT, 2],
+                [utilities.Command.PEN_DOWN, 0],
+                [utilities.Command.PEN_RIGHT, 0],
+                [utilities.Command.PEN_UP, 0],
+                [utilities.Command.PEN_RIGHT, 2],
+
+                [utilities.Command.PEN_DOWN, 0],
+                [utilities.Command.PEN_RIGHT, 0],
+                [utilities.Command.PEN_UP, 0],
+                [utilities.Command.PEN_RIGHT, 2],
+                [utilities.Command.PEN_DOWN, 0],
+                [utilities.Command.PEN_RIGHT, 0],
+                [utilities.Command.PEN_UP, 0],
+                [utilities.Command.PEN_RIGHT, 2],
+
+                [utilities.Command.PEN_DOWN, 0],
+                [utilities.Command.PEN_RIGHT, 0],
+                [utilities.Command.PEN_UP, 0],
+                [utilities.Command.PEN_RIGHT, 1],
+
+                [utilities.Command.PEN_LEFT, x_res - 1],
+                [utilities.Command.SCROLL, 1],
+        ] * x_res
+
+        p_codes = utilities.binarized_image_to_p_codes(binarized, x_res, y_res)
+        self.assertEqual(p_codes, expected_p_codes)

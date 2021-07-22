cf3893552c586b01446e160fc22ecd71479cf780
Piotr Kozka <piotoor@gmail.com>
2021-07-23 01:44:20 +0200

    Bugfix and warning fix in print.sh

--- print.sh
+++ print.sh
@@ -2,11 +2,13 @@
 
 if(($#==2));
 then
-  scp "$2" robot@10.42.0.3:/home/robot/
-  ssh robot@10.42.0.3 "python3 main.py -f \$1 -p \$2"
+  scp "$1" robot@10.42.0.3:/home/robot/
+  # shellcheck disable=SC2029
+  ssh robot@10.42.0.3 "python3 main.py -f $1 -p $2 | tee log.txt"
 elif(($#==1));
 then
-  ssh robot@10.42.0.3 "python3 main.py -p $1"
+  # shellcheck disable=SC2029
+  ssh robot@10.42.0.3 "python3 main.py -p $1 | tee log.txt"
 else
-  ssh robot@10.42.0.3 "python3 main.py -h"
+  ssh robot@10.42.0.3 "python3 main.py -h  | tee log.txt"
 fi
